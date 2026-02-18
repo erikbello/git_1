84d8cc4b8169138d8d179dde96cfc58e332732a4
erikbello <erikbello120721@gmail.com>
2026-02-18 08:11:23 -0500

    funcion que retorna mi nombre con un saludo

---
+++ datos.js
@@ -0,0 +1,17 @@
+const nombre=(nombre)=>'Hola' + " " + nombre;
+console.log(nombre("Erik"));
+
+
+/* 1- gin init
+2-GIT  status ejecuta dos veces 
+3- git add . con punto pasa todos los archivos
+4 - git resetgit
+
+
+
+
+
+
+
+
+*/
